da86e08ae30cefb0136bc4241d2e187d3a8f075c
Koh Shan Chun <s10243956@connect.np.edu.sg>
2024-06-01 23:50:14 +0800

    Add item to list function included

--- app/src/main/java/sg/edu/np/mad/grocerylist/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/grocerylist/MainActivity.java
@@ -1,6 +1,13 @@
 package sg.edu.np.mad.grocerylist;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,73 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> items;
+    ArrayAdapter<String> adapter;
+
+    EditText input;
+    ImageView enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        listView = findViewById(R.id.shopping_listView);
+        input = findViewById(R.id.input);
+        enter = findViewById(R.id.add);
+
+        items = new ArrayList<>();
+        items.add("Apple");
+        items.add("Banana");
+        items.add("Orange");
+        items.add("Strawberry");
+        items.add("Kiwi");
+        items.add("Mango");
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                String name = items.get(i);
+            }
         });
+
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = input.getText().toString();
+
+                if(text == null || text.length() == 0){
+                    makeToast("Please enter an item.");
+                }
+
+                else{
+                    addItem(text);
+                    input.setText("");
+                    makeToast("Added: " + text);
+                }
+            }
+        });
+    }
+
+    public void addItem(String item){
+        items.add(item);
+        listView.setAdapter(adapter);
+    }
+
+    Toast t;
+
+    private void makeToast(String s) {
+        if (t != null) t.cancel();
+        t = Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT);
+        t.show();
     }
 }
