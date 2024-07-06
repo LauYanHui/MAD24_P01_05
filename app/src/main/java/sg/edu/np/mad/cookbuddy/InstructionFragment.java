6a14472d556a7fded6e3f9eeac6ce86e035b3713
LauYanHui <S10241639@connect.np.edu.sg>
2024-07-06 16:17:40 +0800

    Added Text To speech

--- app/src/main/java/sg/edu/np/mad/cookbuddy/InstructionFragment.java
+++ app/src/main/java/sg/edu/np/mad/cookbuddy/InstructionFragment.java
@@ -1,23 +1,31 @@
 package sg.edu.np.mad.cookbuddy;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.List;
 
-public class InstructionFragment extends Fragment {
+public class InstructionFragment extends Fragment implements OnInitListener {
     private static final String ARG_INSTRUCTION_LIST = "instructionList";
+    private static final String TAG = "InstructionFragment";
 
     private List<String> instructionList;
+    private TextToSpeech textToSpeech;
+    private ViewPager2 instructionsPager;
+    private Button speakButton;
 
     public InstructionFragment() {
         // Required empty public constructor
@@ -37,19 +45,58 @@ public class InstructionFragment extends Fragment {
         if (getArguments() != null) {
             instructionList = getArguments().getStringArrayList(ARG_INSTRUCTION_LIST);
         }
+        // Initialize TextToSpeech
+        textToSpeech = new TextToSpeech(getContext(), this);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_instruction, container, false);
 
-        ViewPager2 instructionsPager = view.findViewById(R.id.instructionsPager);
+        instructionsPager = view.findViewById(R.id.instructionsPager);
+        speakButton = view.findViewById(R.id.speakButton);
+
         if (instructionList != null) {
             InstructionsPagerAdapter adapter = new InstructionsPagerAdapter(instructionList);
             instructionsPager.setAdapter(adapter);
         }
 
+        speakButton.setOnClickListener(v -> {
+            int currentItem = instructionsPager.getCurrentItem();
+            if (currentItem < instructionList.size()) {
+                String instruction = instructionList.get(currentItem);
+                speakInstruction(instruction);
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int langResult = textToSpeech.setLanguage(Locale.US);
+            if (langResult == TextToSpeech.LANG_MISSING_DATA || langResult == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e(TAG, "Language is not supported or missing data");
+            }
+        } else {
+            Log.e(TAG, "Initialization failed");
+        }
+    }
+
+    private void speakInstruction(String instruction) {
+        if (textToSpeech != null) {
+            textToSpeech.speak(instruction, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
     }
 }
