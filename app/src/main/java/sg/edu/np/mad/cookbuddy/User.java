379d7927cdb9a26ad9f0355c738b3d65933e1fc6
JunHao-NP <S10262731@connect.np.edu.sg>
2024-07-07 16:35:51 +0800

    Added bookmark and favourites list into user

--- app/src/main/java/sg/edu/np/mad/cookbuddy/User.java
+++ app/src/main/java/sg/edu/np/mad/cookbuddy/User.java
@@ -8,15 +8,23 @@ public class User implements Serializable {
     private String username;
     private String password;
     private ArrayList<String> allergies;
+    private ArrayList<String> bookmarks;
+    private ArrayList<String> favorites;
     public User(String username, String password, ArrayList<String> allergies) {
         this.username = username;
         this.password = password;
         this.allergies = new ArrayList<String>();
+        this.bookmarks = new ArrayList<>();
+        this.favorites = new ArrayList<>();
     }
     public String getUsername() { return this.username; }
     public String getPassword() { return this.password; }
     public List<String> getAllergies() { return this.allergies; }
+    public List<String> getBookmarks() { return bookmarks; }
+    public List<String> getFavorites() { return favorites; }
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
     public void setAllergies(ArrayList<String> allergies) { this.allergies = allergies; }
+    public void setBookmarks(ArrayList<String> bookmarks) { this.bookmarks = bookmarks; }
+    public void setFavorites(ArrayList<String> favorites) { this.favorites = favorites; }
 }
