10ff52bf71e6f43dd1d123776d0c90ae02b57f74
Ryan <s10261024@connect.np.edu.sg>
2024-07-16 12:17:07 +0800

    feat: update user class

--- app/src/main/java/sg/edu/np/mad/cookbuddy/models/User.java
+++ app/src/main/java/sg/edu/np/mad/cookbuddy/models/User.java
@@ -8,17 +8,28 @@ public class User implements Serializable {
     private String username;
     private String password;
     private List<String> allergies;
+    private List<String> favourites;
 
     public User(String username, String password, List<String> allergies) {
         this.username = username;
         this.password = password;
         this.allergies = allergies;
+        this.favourites = new ArrayList<String>();
+    }
+
+    public User(String username, String password, List<String> allergies, List<String> favourites) {
+        this.username = username;
+        this.password = password;
+        this.allergies = allergies;
+        this.favourites = favourites;
     }
 
     public String getUsername() { return this.username; }
     public String getPassword() { return this.password; }
     public List<String> getAllergies() { return this.allergies; }
+    public List<String> getFavourites() { return this.favourites; }
     public void setUsername(String username) { this.username = username; }
     public void setPassword(String password) { this.password = password; }
-    public void setAllergies(ArrayList<String> allergies) { this.allergies = allergies; }
+    public void setAllergies(List<String> allergies) { this.allergies = allergies; }
+    public void setFavourites(List<String> favourites) { this.favourites = favourites; }
 }
