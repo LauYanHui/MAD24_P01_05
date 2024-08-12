068da4e2a43fb847c760e5e65a0c147cc472b595
Koh Shan Chun <s10243956@connect.np.edu.sg>
2024-08-12 10:01:43 +0800

    Removing

--- app/src/main/java/sg/edu/np/mad/grocerylist/StoreLocatorActivity.java
+++ app/src/main/java/sg/edu/np/mad/grocerylist/StoreLocatorActivity.java
@@ -7,22 +7,14 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
-import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationRequest;
 import android.os.Bundle;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -36,16 +28,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import sg.edu.np.mad.grocerylist.databinding.ActivityStoreLocatorBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class StoreLocatorActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
-GoogleApiClient.OnConnectionFailedListener, LocationListener {
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap;
     private ActivityStoreLocatorBinding binding;
 
-    GoogleApiClient mGoogleApiClient;
-    double currentLatitude,currentLongitude;
-    Location myLocation;
+    private GoogleApiClient mGoogleApiClient;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private double currentLatitude, currentLongitude;
+    private Location myLocation;
 
     private final static int REQUEST_CHECK_SETTINGS_GPS = 0x1;
     private final static int REQUEST_ID_MULTIPLE_PERMISSIONS = 0x2;
@@ -57,17 +52,20 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
         binding = ActivityStoreLocatorBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
 
         setUPGClient();
     }
 
     private void setUPGClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
-            .enableAutoManage(this, 0, this)
+                .enableAutoManage(this, 0, this)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
@@ -78,11 +76,6 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        //LatLng sydney = new LatLng(-34, 151);
-        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     @Override
@@ -94,49 +87,44 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
         int permissionLocation = ContextCompat.checkSelfPermission(StoreLocatorActivity.this, Manifest.permission.ACCESS_FINE_LOCATION);
 
         List<String> listPermission = new ArrayList<>();
-        if (permissionLocation != PackageManager.PERMISSION_GRANTED){
+        if (permissionLocation != PackageManager.PERMISSION_GRANTED) {
             listPermission.add(Manifest.permission.ACCESS_FINE_LOCATION);
 
-            if (!listPermission.isEmpty()){
+            if (!listPermission.isEmpty()) {
                 ActivityCompat.requestPermissions(this,
-                        listPermission.toArray(new String[listPermission.size()]),REQUEST_ID_MULTIPLE_PERMISSIONS);
+                        listPermission.toArray(new String[0]), REQUEST_ID_MULTIPLE_PERMISSIONS);
             }
-
-
-        }
-        else {
+        } else {
             getMyLocation();
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        int permissionLocation = ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults); // Add this line
 
-        if (permissionLocation == PackageManager.PERMISSION_GRANTED){
+        int permissionLocation = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+
+        if (permissionLocation == PackageManager.PERMISSION_GRANTED) {
             getMyLocation();
-        }
-
-        else {
+        } else {
             checkPermission();
         }
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
         myLocation = location;
 
-        if (myLocation != null){
+        if (myLocation != null) {
             currentLatitude = location.getLatitude();
             currentLongitude = location.getLongitude();
 
@@ -144,7 +132,7 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLatitude, currentLongitude), 15.0f));
             MarkerOptions markerOptions = new MarkerOptions();
-            markerOptions.position(new LatLng(currentLatitude,currentLongitude));
+            markerOptions.position(new LatLng(currentLatitude, currentLongitude));
             markerOptions.title("You are Here");
             markerOptions.icon(icon);
             mMap.addMarker(markerOptions);
@@ -153,16 +141,16 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
         }
     }
 
-    private void getNearbySupermarkets(){
+    private void getNearbySupermarkets() {
         StringBuilder stringBuilder = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
-        stringBuilder.append("location="+String.valueOf(currentLatitude)+ "," +String.valueOf(currentLongitude));
+        stringBuilder.append("location=").append(currentLatitude).append(",").append(currentLongitude);
         stringBuilder.append("&radius=1500");
         stringBuilder.append("&type=supermarket");
-        stringBuilder.append("&key="+getResources().getString(R.string.google_maps_key));
+        stringBuilder.append("&key=").append(getResources().getString(R.string.google_maps_key));
 
         String url = stringBuilder.toString();
 
-        Object dataTransfer[] = new Object[2];
+        Object[] dataTransfer = new Object[2];
         dataTransfer[0] = mMap;
         dataTransfer[1] = url;
 
@@ -170,76 +158,31 @@ GoogleApiClient.OnConnectionFailedListener, LocationListener {
         getNearbyPlacesData.execute(dataTransfer);
     }
 
-    private void getMyLocation(){
-        if(mGoogleApiClient!=null) {
-            if (mGoogleApiClient.isConnected()) {
-                int permissionLocation = ContextCompat.checkSelfPermission(StoreLocatorActivity.this,
-                        Manifest.permission.ACCESS_FINE_LOCATION);
-                if (permissionLocation == PackageManager.PERMISSION_GRANTED) {
-                    myLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-                    LocationRequest locationRequest = LocationRequest.create();
-                    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-                    locationRequest.setInterval(10000); // 10 seconds
-                    locationRequest.setFastestInterval(5000); // 5 seconds
-                    LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                            .addLocationRequest(locationRequest);
-                    builder.setAlwaysShow(true);
-                    LocationServices.FusedLocationApi
-                            .requestLocationUpdates(mGoogleApiClient, locationRequest, this);
-                    PendingResult<LocationSettingsResult> result =
-                            LocationServices.SettingsApi
-                                    .checkLocationSettings(mGoogleApiClient, builder.build());
-                    result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
-
-                        @Override
-                        public void onResult(LocationSettingsResult result) {
-                            final Status status = result.getStatus();
-                            switch (status.getStatusCode()) {
-                                case LocationSettingsStatusCodes.SUCCESS:
-                                    // All location settings are satisfied.
-                                    // You can initialize location requests here.
-                                    int permissionLocation = ContextCompat
-                                            .checkSelfPermission(StoreLocatorActivity.this,
-                                                    Manifest.permission.ACCESS_FINE_LOCATION);
-                                    if (permissionLocation == PackageManager.PERMISSION_GRANTED) {
 
 
-                                        myLocation = LocationServices.FusedLocationApi
-                                                .getLastLocation(mGoogleApiClient);
+    private void getMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        myLocation = location;
+                        currentLatitude = location.getLatitude();
+                        currentLongitude = location.getLongitude();
 
+                        BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.drawable.navigation);
 
-                                    }
-                                    break;
-                                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                                    // Location settings are not satisfied.
-                                    // But could be fixed by showing the user a dialog.
-                                    try {
-                                        // Show the dialog by calling startResolutionForResult(),
-                                        // and check the result in onActivityResult().
-                                        // Ask to turn on GPS automatically
-                                        status.startResolutionForResult(StoreLocatorActivity.this,
-                                                REQUEST_CHECK_SETTINGS_GPS);
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLatitude, currentLongitude), 15.0f));
+                        MarkerOptions markerOptions = new MarkerOptions();
+                        markerOptions.position(new LatLng(currentLatitude, currentLongitude));
+                        markerOptions.title("You are Here");
+                        markerOptions.icon(icon);
+                        mMap.addMarker(markerOptions);
 
-
-                                    } catch (IntentSender.SendIntentException e) {
-                                        // Ignore the error.
-                                    }
-
-
-                                    break;
-                                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                                    // Location settings are not satisfied.
-                                    // However, we have no way
-                                    // to fix the
-                                    // settings so we won't show the dialog.
-                                    // finish();
-                                    break;
-                            }
-                        }
-                    });
-
+                        getNearbySupermarkets();
+                    }
                 }
-            }
+            });
         }
     }
 }
